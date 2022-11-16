c4cc282a7d9bfd268bbe6032a912ab5203a5cdd5
Adriano <Adriano@DESKTOP-5PJ4CG7>
2022-11-16 09:20:39 -0500

    Agregar archivos de proyecto.

---
+++ Sema6.Android/MensajeAndroid.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Sema6.Droid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+[assembly:Xamarin.Forms.Dependency(typeof(MensajeAndroid))]
+namespace Sema6.Droid
+{
+      internal class MensajeAndroid : Mensaje
+    {
+        public void LongAlert(string mensaje)
+        {
+            Toast.MakeText(Application.Context, mensaje, ToastLength.Long).Show();
+        }
+
+        public void ShortAlert(string mensaje)
+        {
+            Toast.MakeText(Application.Context, mensaje, ToastLength.Short).Show();
+
+        }
+    }
+}
---
+++ Sema6/Mensaje.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sema6
+{
+    public interface Mensaje
+    {
+        void LongAlert(string mensaje);
+        void ShortAlert(string mensaje);
+    }
+}
